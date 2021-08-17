130a2138650996072bc813ac72271e15c404c579
Ramon <esqueletopvp@gmail.com>
2021-08-16 19:01:08 -0600

    solucionando datos de fallo de test

--- MySMLJRPE/boards/views.py
+++ MySMLJRPE/boards/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 
 from .models import Board
 
@@ -7,5 +7,6 @@ def home(request):
     return render(request, 'home.html', {'boards': boards})
 
 def board_topics(request, pk):
+    board=get_list_or_404(Board,pk=pk)
     board = Board.objects.get(pk=pk)
     return render(request, 'topics.html', {'board': board})
