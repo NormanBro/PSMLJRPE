9b7dcf4063658c437fb3c7798eaa340e8d5c46ce
Ramon <esqueletopvp@gmail.com>
2021-08-17 19:12:31 -0600

    update view get queryset

--- MySMLJRPE/boards/views.py
+++ MySMLJRPE/boards/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404,redirect
 from django.contrib.auth.models import User
 from django.db.models import Count
+
 from .models import Board,Topic,Post
 from .forms import NewTopicForm,PostForm
+
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 from django.views.generic import UpdateView
 from django.utils import timezone
+from django.utils.decorators import method_decorator
 
 def home(request):
     boards = Board.objects.all()
@@ -79,12 +82,17 @@ def new_topic(request, pk):
     return render(request, 'new_topic.html', {'board': board, 'form': form})
 """
 
+@method_decorator(login_required, name='dispatch')
 class PostUpdateView(UpdateView):
     model = Post
     fields = ('message', )
     template_name = 'edit_post.html'
     pk_url_kwarg = 'post_pk'
     context_object_name = 'post'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(created_by=self.request.user)
 
     def form_valid(self, form):
         post = form.save(commit=False)
