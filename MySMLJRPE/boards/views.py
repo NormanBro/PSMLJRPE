e9d4849139033dac20be52666f25ec412dfeb20a
Ramon <esqueletopvp@gmail.com>
2021-08-17 19:03:46 -0600

    edit post

--- MySMLJRPE/boards/views.py
+++ MySMLJRPE/boards/views.py
@@ -4,6 +4,9 @@ from django.db.models import Count
 from .models import Board,Topic,Post
 from .forms import NewTopicForm,PostForm
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from django.views.generic import UpdateView
+from django.utils import timezone
 
 def home(request):
     boards = Board.objects.all()
@@ -75,3 +78,17 @@ def new_topic(request, pk):
         form = NewTopicForm()
     return render(request, 'new_topic.html', {'board': board, 'form': form})
 """
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ('message', )
+    template_name = 'edit_post.html'
+    pk_url_kwarg = 'post_pk'
+    context_object_name = 'post'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.updated_by = self.request.user
+        post.updated_at = timezone.now()
+        post.save()
+        return redirect('topic_posts', pk=post.topic.board.pk, topic_pk=post.topic.pk)
